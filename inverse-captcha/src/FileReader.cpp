32bd6366c0e1aa4e6a8ed363fbd66e4161dc2c94
Loïc Benet <loic.benet.jca@gmail.com>
2017-12-21 13:07:16 +0100

    Adding the true problem solving and removed debugging messages

--- inverse-captcha/src/FileReader.cpp
+++ inverse-captcha/src/FileReader.cpp
@@ -6,21 +6,18 @@
  */
 
 #include "FileReader.h"
-#include <iostream>
 
 std::vector<int> InverseCaptcha::FileReader::read(std::string const& iFile) const {
     auto ifs    = std::ifstream(iFile);
     auto data   = std::vector<int>();
     auto c      = 'a';
-    if (ifs.is_open())
-        std::cout << iFile << " opened.\n";
-    while (ifs.get(c)) {
-        data.push_back(static_cast<int>(c - '0'));
-        std::cout << static_cast<int>(c - '0') << " pushed.\n";
+    if (ifs.is_open()) {
+        while (ifs.get(c)) {
+            data.push_back(static_cast<int>(c - '0'));
+        }
     }
     // Remove eof.
     data.pop_back();
     ifs.close();
-    std::cout << iFile << " closed.\n";
     return data;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
  * Distributed under terms of the GPLv3 license.
  */
 
+#include <iostream>
+#include <vector>
+#include "CaptchaSolver.h"
+#include "FileReader.h"
+
 int main() {
+    std::cout << "Solving inverse captcha problem...\n";
+    auto const reader   = InverseCaptcha::FileReader();
+    auto const data     = reader.read("resources/inverse-captcha");
+    auto const solver   = InverseCaptcha::CaptchaSolver({ std::make_move_iterator(data.begin()), std::make_move_iterator(data.end()) });
+    std::cout << "Result is " << solver.solve() << ".\n";
     return 0;
 }
